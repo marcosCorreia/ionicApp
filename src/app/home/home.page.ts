21f97e82779c19cda1e83ae61fe3863ac9e94544
marcos Jr <47080835+marcosCorreia@users.noreply.github.com>
2020-04-02 15:52:42 -0300

    App de exemplo
    Exemplo de app que calcula a gasolina

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private navegacao: NavController) {}
 
+  ngOnInit() {
+
+  }
+
+  public pesquisa: String = ""
+  public resultado: String = ""
   public titulo: String = "Meu primeiro App"
   public imagemRandomica: String = "https://source.unsplash.com/random/200x200"
   
   public imagemLocal: String = "../assets/icone-celular.png"
+
+  recuperar() {
+    this.resultado = this.pesquisa
+  }
+  abrirBotoes() {
+    this.navegacao.navigateForward('botoes')
+  }
+
+  abrirLista() {
+    this.navegacao.navigateForward('lista')
+  }
 
   public atualiza(): void {
 
